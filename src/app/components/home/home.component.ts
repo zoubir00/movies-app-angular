177a7ff1a5265c8491bfb325f93f2adf63419cc1
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-09-13 16:06:29 +0100

    attribute directive ngStyle

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  errorColor='text-danger';
-  successColor='text-success';
-  isSuccess=true;
+  // errorColor='text-danger';
+  // successColor='text-success';
+  // isSuccess=true;
+
+  currentStyles:Record<string,string>={};
+
+  IsCentered=true;
+  IsSuccess=true;
+  IsLarge=true;
+  constructor(){
+    this.currentStyles={
+      'text-align': this.IsCentered ? 'center':'',
+      'color': this.IsSuccess ? 'green' : 'red',
+      'font-size': this.IsLarge ? 'large' : 'small',
+    }
+  }
+
+
 
 
 }
